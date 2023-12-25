aaed6327bdbce5010d2bf9fc99d3b911bb84d1eb
Patel-priyankaa <153794761+Patel-priyankaa@users.noreply.github.com>
2023-12-25 14:29:33 +0530

    Add files via upload

---
+++ TopNav.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const TopNav = () => {
+  return (
+    <>
+      <section>
+        <div className="container-fluid" style={{ backgroundColor: "#f5f5f5" }}>
+          <div className="top center" style={{ fontSize: "17px" }}>
+            <span style={{ color: "#000000", fontWeight: "400" }}>
+              {"  Send an E-Gift Card for easy last-minute gifting."}
+            </span>
+            <a href="/" className="hover_text" style={{ fontWeight: "600" }}>
+              {" Shop E-Gift Cards"}
+            </a>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default TopNav;
